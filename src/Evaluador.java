248d7b9cf683bac7d8351efd325927c1c07747f5
Luis Paredes <lualps94@gmail.com>
2018-12-08 23:01:49 -0400

    Created initial structure for Evaluador class

--- src/Evaluador.java
+++ src/Evaluador.java
@@ -41,8 +41,60 @@ public class Evaluador {
 		return result;
 	}
 
+	/**
+	 * Construye un árbol de sintaxis abstracta a partir de una expresión en
+	 * notación polaca reversa.
+	 */
+	public static void buildExprGraphForTheLulz(String reversedPolishExpr) {
+
+	}
+
+	/**
+	 * Aplica el algoritmo shunting-yard a la expresión dada para retornar la
+	 * misma expresión en notación polaca reversa.
+	 */
+	private static String convertToReversedPolish(String expr) {
+		return "";
+	}
+
+	/**
+	 * Evalua una expresión en notación polaca reversa.
+	 */
+	public static int eval(String reversedPolishExpr) {
+		return 0;
+	}
+
+
+	/**
+	 * Importa un archivo que contiene expresiones y en caso de que estas
+	 * expresiones sean válidas, imprime los resultados en la cónsola.
+	 * <br><br>
+	 * También llama a la función que crea el grafo asociado a cada expresión.
+	 * Nótese que dicha llamada se hace únicamente para cumplir con la
+	 * especificación del proyecto, sin embargo, su ejecución es irrelevante
+	 * para los efectos de las funcionalidades que ofrece la aplicación.
+	 */
+	public static void importAndEval(String file) {
+
+		String[] expressions = getExpressionsFrom(file);
+
+		int result;
+		for (String expr : expressions) {
+			String reversedPolishExpr = convertToReversedPolish(expr);
+			result = eval(reversedPolishExpr);
+			buildExprGraphForTheLulz(reversedPolishExpr);
+			System.out.print(result);
+		}
+	}
 
 	public static void main(String[] args) {
-		Evaluador e = new Evaluador();
+		if (args.length != 1) {
+			System.out.println(
+				"Debe proporcionar únicamente el nombre del archivo de entrada"+
+				" como argumento"
+			);
+			System.exit(0);
+		}
+		importAndEval(args[0]);
 	}
 }
